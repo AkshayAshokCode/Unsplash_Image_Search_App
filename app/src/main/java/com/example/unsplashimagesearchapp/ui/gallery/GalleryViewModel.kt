e246dd66d65720e2436d2fe433025ed430a30a82
Riderpool1 <akshayashokan1054.com>
2020-11-22 12:01:20 +0530

    Details Fragment added

--- app/src/main/java/com/example/unsplashimagesearchapp/ui/gallery/GalleryViewModel.kt
+++ app/src/main/java/com/example/unsplashimagesearchapp/ui/gallery/GalleryViewModel.kt
@@ -1,24 +1,28 @@
 package com.example.unsplashimagesearchapp.ui.gallery
 
+import androidx.hilt.Assisted
 import androidx.hilt.lifecycle.ViewModelInject
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.switchMap
-import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.*
 import androidx.paging.cachedIn
 import com.example.unsplashimagesearchapp.data.UnsplashRepository
 
-class GalleryViewModel @ViewModelInject constructor(private val repository: UnsplashRepository) :
+class GalleryViewModel @ViewModelInject constructor(
+    private val repository: UnsplashRepository,
+    @Assisted state: SavedStateHandle
+) :
     ViewModel() {
-    private val currentQuery= MutableLiveData(DEFAULT_QUERY)
+    private val currentQuery = state.getLiveData(CURRENT_QUERY, DEFAULT_QUERY)
 
-    val photos=currentQuery.switchMap{ queryString ->
+    val photos = currentQuery.switchMap { queryString ->
         repository.getSearchResults(queryString).cachedIn(viewModelScope)
     }
-    fun searchPhotos(query:String){
-        currentQuery.value=query
+
+    fun searchPhotos(query: String) {
+        currentQuery.value = query
     }
-    companion object{
-        private const val DEFAULT_QUERY=",messi"
+
+    companion object {
+        private const val CURRENT_QUERY = "current_query"
+        private const val DEFAULT_QUERY = "nature"
     }
 }
